c27f84d9505a4a5b655ef1a02fd22c078fad377c
Roman Egoshin <egoshin.roman@gmail.com>
2021-09-16 22:08:46 +0200

    Removed critical section in the <esp_timer_get_status>

--- components/esp_timer/src/esp_timer.c
+++ components/esp_timer/src/esp_timer.c
@@ -187,11 +187,7 @@ esp_err_t IRAM_ATTR esp_timer_stop(esp_timer_handle_t timer)
 
 bool esp_timer_get_status(esp_timer_handle_t timer)
 {
-  bool status;
-  timer_list_lock();
-  status = timer_armed(timer);
-  timer_list_unlock();
-  return status;
+  return timer_armed(timer);
 }
 
 esp_err_t esp_timer_delete(esp_timer_handle_t timer)
